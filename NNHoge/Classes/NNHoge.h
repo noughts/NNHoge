afcbef62cf2e5e568345429fff82dfbc9bd9c4e2
Koichi Yamamoto <noughts@gmail.com>
2016-09-25 17:00:02 +0900

    a

---
+++ NNHoge/Classes/NNHoge.h
@@ -0,0 +1,13 @@
+//
+//  NNHoge.h
+//  Pods
+//
+//  Created by noughts on 2016/09/25.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NNHoge : NSObject
+
+@end
---
+++ NNHoge/Classes/NNHoge.m
@@ -0,0 +1,13 @@
+//
+//  NNHoge.m
+//  Pods
+//
+//  Created by noughts on 2016/09/25.
+//
+//
+
+#import "NNHoge.h"
+
+@implementation NNHoge
+
+@end
